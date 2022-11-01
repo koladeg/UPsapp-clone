4d1b2282ddaa07a33dbb0f7026fdbb4e5bfba097
koladeg <tayokay@yahoo.com>
2022-11-01 20:32:31 +0100

    add customer screen

---
+++ constants/Colors.tsx
@@ -0,0 +1,4 @@
+export default {
+    primary: "#59C1CC",
+    secondary: "#EB6A7C",
+}
--- navigator/TabNavigator.tsx
+++ navigator/TabNavigator.tsx
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, {useLayoutEffect} from 'react';
 import { View, Text } from 'react-native';
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import CustomersScreen from "../screens/Customers";
 import OrdersScreen from "../screens/OrdersScreen";
+import { useNavigation } from '@react-navigation/native';
+import { Icon } from '@rneui/themed';
+import Colors from "../constants/Colors";
 
-const Tab = createBottomTabNavigator();
+export type TabStackParamList = {
+    Customers: undefined;
+    Orders: undefined
+}
+
+const Tab = createBottomTabNavigator<TabStackParamList>();
 
 const TabNavigator = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        })
+    })
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={CustomersScreen} />
-            <Tab.Screen name="Home" component={OrdersScreen} />
+        <Tab.Navigator screenOptions={({ route }) =>({
+            tabBarActiveTintColor: Colors.primary,
+            tabBarInactiveTintColor: "gray",
+            tabBarIcon: ({ focused, color, size}) => {
+                if (route.name === 'Customers'){
+                    return (
+                        <Icon
+                            name="users"
+                            type="entypo"
+                            color={focused ? Colors.primary : "gray"}
+                        />
+                    );
+                } else if ( route.name === 'Orders') {
+                    return (
+                        <Icon
+                            name="box"
+                            type="entypo"
+                            color={focused ? Colors.secondary : "gray"}
+                        />
+                    );
+                }
+            }
+        })}>
+            <Tab.Screen name="Customers" component={CustomersScreen} />
+            <Tab.Screen name="Orders" component={OrdersScreen} />
         </Tab.Navigator>
     );
 }
