344c598522a027ff501536920fda1ae4def671e0
koladeg <tayokay@yahoo.com>
2022-11-05 15:00:27 +0100

    add graphql

--- App.tsx
+++ App.tsx
@@ -4,14 +4,22 @@ import {TailwindProvider} from 'tailwind-rn';
 import utilities from './tailwind.json';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from "./navigator/RootNavigator";
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:5001/api/lanky-wolf',
+    cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
       // @ts-ignore - TailwindProvider is missing a type definition
       <TailwindProvider utilities={utilities}>
-          <NavigationContainer>
-              <RootNavigator />
-          </NavigationContainer>
+          <ApolloProvider client={client}>
+              <NavigationContainer>
+                  <RootNavigator />
+              </NavigationContainer>
+          </ApolloProvider>
       </TailwindProvider>
   );
 }
